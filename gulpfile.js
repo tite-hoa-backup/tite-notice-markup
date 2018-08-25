6f42d16c2cd3fd1345bac26c6c09f0814e4578b7
spemer <ghsspower@naver.com>
2018-08-25 14:41:09 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+"use strict";
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      watch = require('gulp-watch'),
+      inject = require('gulp-inject'),
+      imagemin = require('gulp-imagemin'),
+      minifyjs = require('gulp-js-minify'),
+      cleanCSS = require('gulp-clean-css'),
+      autoprefixer = require('gulp-autoprefixer'),
+      browserSync = require('browser-sync').create();
+
+
+/* ==============================
+    static server
+============================== */
+gulp.task('browser-sync', function() {
+    browserSync.init(['*'], {
+        files: [
+            './*',
+            './html/*',
+            './html/**/*',
+            './css/src/*',
+            './css/dist/*',
+            './js/**/*',
+            './images/src/*',
+            './images/dist/*',
+        ],
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('*').on('change', browserSync.reload);
+});
+
+
+/* ==============================
+    sass sync watch
+============================== */
+gulp.task('sass', function () {
+    return gulp.src('./css/src/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./css/tmp'))
+        .pipe(autoprefixer({
+            browsers: ['cover 99.5%', 'ie 6-8'],
+            cascade: false
+        }))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./css/src/*.scss', ['sass']);
+});
+
+
+/* ==============================
+    css autoprefixer & minify -> tmp to dist
+============================== */
+gulp.task('prefix-css', function () {
+    return watch('./css/tmp/*.css', { ignoreInitial: false })
+        .pipe(autoprefixer({
+            browsers: ['cover 99.5%', 'ie 6-8'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./css/dist'));
+});
+
+
+/* ==============================
+    minify js
+============================== */
+gulp.task('jsmin', function () {
+    return watch('./js/src/*.js', function () {
+        gulp.src('./js/src/*.js')
+            .pipe(minifyjs())
+            .pipe(gulp.dest('./js/dist'))
+            .pipe(browserSync.reload({stream: true}));
+    });
+});
+
+
+/* ==============================
+    image resizer
+============================== */
+gulp.task('img-resize', () =>
+	gulp.src('./img/src/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./img/dist'))
+        .pipe(browserSync.reload({stream: true}))
+);
+
+
+/* ==============================
+    inject assets
+============================== */
+gulp.task('inject', function () {
+    var target  = gulp.src(['./*.html']);
+    var sources = gulp.src(['./js/dist/index.js',
+                            './js/dist/polyfill.js',
+                            './css/dist/*.css'], {read: false});
+
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+/* ==============================
+    gulp watch
+============================== */
+gulp.task('watch', ['browser-sync',
+                    'sass:watch',
+                    'prefix-css',
+                    'jsmin',
+                    'img-resize',
+                    'inject'])
---
+++ js/src/index.js
@@ -0,0 +1,103 @@
+/* ==============================
+    meta Tags **meta [name, content]**
+============================== */
+(function(){
+    var thisUrl = window.location.href;
+    var thisTitle = document.title;
+    var thisDesc = document.querySelector('meta[name="description"]').getAttribute('content');
+    var thisKeys = document.querySelector('meta[name="keywords"]').getAttribute('content');
+    var thisOGImg = document.querySelector('meta[property="og:image"]').getAttribute('content');
+
+    var metaLists = [
+        { name: 'url', content: thisUrl },
+        { name: 'identifier-URL', content: thisUrl },
+        { name: 'pagename', content: thisTitle },
+        { name: 'HandheldFriendly', content: 'true' },
+        { name: 'referrer', content: 'origin-when-cross-origin' },
+        { name: 'twitter:url', content: thisUrl },
+        { name: 'twitter:domain', content: thisUrl },
+        { name: 'twitter:title', content: thisTitle },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:description', content: thisDesc },
+        { name: 'twitter:image', content: thisOGImg },
+        { name: 'twitter:image:src', content: thisOGImg },
+        { name: 'twitter:image:alt', content: thisTitle },
+        { property: 'og:img:alt', content: thisOGImg },
+        { property: 'og:img:url', content: thisOGImg },
+        { property: 'og:img:secure_url', content: thisOGImg },
+        { property: 'article:tag', content: thisKeys },
+        { property: 'al:web:url', content: thisUrl },
+        { itemprop: 'name', content: thisTitle },
+        { itemprop: 'description', content: thisDesc },
+        { itemprop: 'image', content: thisOGImg },
+        { rel: 'apple-touch-icon', sizes: '57x57' },
+        { rel: 'apple-touch-icon', sizes: '60x60' },
+        { rel: 'apple-touch-icon', sizes: '72x72' },
+        { rel: 'apple-touch-icon', sizes: '76x76' },
+        { rel: 'apple-touch-icon', sizes: '114x114' },
+        { rel: 'apple-touch-icon', sizes: '120x120' },
+        { rel: 'apple-touch-icon', sizes: '144x144' },
+        { rel: 'apple-touch-icon', sizes: '152x152' },
+        { rel: 'apple-touch-icon', sizes: '180x180' },
+        { rel: 'icon', sizes: '192x192' },
+        { rel: 'icon', sizes: '32x32' },
+        { rel: 'icon', sizes: '96x96' },
+        { rel: 'icon', sizes: '16x16' },
+        { rel: 'manifest', href: '/favicon/manifest.json' },
+        { name: 'msapplication-TileColor', content: '#ffffff' },
+        { name: 'msapplication-TileColor', content: '/favicon/ms-icon-144x144.png' },
+        { name: 'theme-color', content: '#ffffff' },
+    ];
+
+    var headTitle = document.querySelector('head');
+    metaLists.forEach(function(metaList) {
+        if (metaList.name) {
+            var setMeta = document.createElement('meta');
+            setMeta.setAttribute('name', metaList.name);
+            setMeta.setAttribute('content', metaList.content);
+            headTitle.appendChild(setMeta);
+        }
+
+        else if (metaList.property) {
+            var setMeta = document.createElement('meta');
+            setMeta.setAttribute('property', metaList.property);
+            setMeta.setAttribute('content', metaList.content);
+            headTitle.appendChild(setMeta);
+        }
+
+        else if (metaList.itemprop) {
+            var setMeta = document.createElement('meta');
+            setMeta.setAttribute('itemprop', metaList.itemprop);
+            setMeta.setAttribute('content', metaList.content);
+            headTitle.appendChild(setMeta);
+        }
+
+        else if (metaList.rel == 'apple-touch-icon') {
+            var setLink = document.createElement('link');
+            setLink.setAttribute('rel', metaList.rel);
+            setLink.setAttribute('sizes', metaList.sizes);
+            setLink.setAttribute('href', '/favicon/apple-icon-' + metaList.sizes + '.png');
+            headTitle.appendChild(setLink);
+        }
+
+        else if (metaList.rel == 'icon' || metaList.rel == 'manifest') {
+            var setLink = document.createElement('link');
+            setLink.setAttribute('rel', metaList.rel);
+
+            if (metaList.rel == 'icon') {
+                setLink.setAttribute('type', 'image/png');
+                setLink.setAttribute('sizes', metaList.sizes);
+
+                if (metaList.sizes == '192x192')
+                    setLink.setAttribute('href', '/favicon/android-icon-' + metaList.sizes + '.png');
+                else
+                    setLink.setAttribute('href', '/favicon/favicon-' + metaList.sizes + '.png');
+                
+            } else {
+                setLink.setAttribute('href', metaList.href);
+            }
+
+            headTitle.appendChild(setLink);
+        }
+    });
+})();
