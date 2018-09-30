602e577e37eff01c38bcbaf9f428f0208752d25c
spemer <ghsspower@naver.com>
2018-10-01 01:26:16 +0900

    FIX ripple style

--- js/src/index.js
+++ js/src/index.js
@@ -170,7 +170,7 @@
             var pos = ripple.getBoundingClientRect();
             var x = e.clientX - pos.left - size / 2;
             var y = e.clientY - pos.top - size / 2;
-            var style = 'top:' + y + 'px;left: ' + x + 'px; height: ' + size + 'px; width: ' + size + 'px;';
+            style = 'will-change: top, left, height, width, auto; zoom: 1; top: ' + y + 'px; left: ' + x + 'px; height: ' + size + 'px; width: ' + size + 'px;';
 
             ripple.rippleContainer.appendChild(setRipple);
 
@@ -187,18 +187,17 @@
 
         for (var i = 0, length = ripples.length; i < length; i++) {
             ripple = ripples[i];
-
-            // set ripple parent style
-            ripple.style.zIndex = '100';
             ripple.style.position = 'relative';
             ripple.style.overflow = 'hidden';
 
             rippleContainer = document.createElement('div');
             rippleContainer.className = 'ripple--container';
-
-            // set ripple container style
+            rippleContainer.style.zoom = '1';
             rippleContainer.style.overflow = 'hidden';
             rippleContainer.style.position = 'absolute';
+            rippleContainer.style.transform = 'translate3d(0, 0, 0)';
+            rippleContainer.style.webkitTransform = 'translate3d(0, 0, 0)';
+            rippleContainer.style.willChange = 'top, left, right, bottom, auto';
             rippleContainer.style.top = '0';
             rippleContainer.style.left = '0';
             rippleContainer.style.right = '0';
@@ -212,6 +211,6 @@
 
         // ripple style
         var styleEl = document.createElement('style');
-        styleEl.innerHTML = '[ripple] .ripple--container .ripple--body {will-change:transform,opacity;overflow:hidden;-webkit-transform:scale(0);-moz-transform:scale(0);-ms-transform:scale(0);-o-transform:scale(0);transform:scale(0);-webkit-border-radius:100%;-moz-border-radius:100%;border-radius:100%;position:absolute;opacity:0.5;background-color:rgba(0,0,0,0.1);-webkit-animation:rippler 1000ms;-moz-animation:rippler 1000ms;-o-animation:rippler 1000ms;animation:rippler 1000ms;}@-webkit-keyframes rippler{to{opacity:0;-webkit-transform:scale(2);transform:scale(2);}}@-moz-keyframes rippler{to{opacity:0;-webkit-transform:scale(2);-moz-transform:scale(2);transform:scale(2);}}@-o-keyframes rippler{to{opacity:0;-webkit-transform:scale(2);-o-transform:scale(2);transform:scale(2);}}@keyframes rippler{to{opacity:0;-webkit-transform:scale(2);-moz-transform:scale(2);-o-transform:scale(2);transform:scale(2);}}';
+        styleEl.innerHTML = '[ripple] .ripple--container span {will-change:transform,opacity,auto;zoom:1;overflow:hidden;-webkit-transform:scale(0);-moz-transform:scale(0);-ms-transform:scale(0);-o-transform:scale(0);transform:scale(0);-webkit-border-radius:100%;-moz-border-radius:100%;border-radius:100%;position:absolute;opacity:0.5;background-color:rgba(0,0,0,0.1);-webkit-animation:rippler 1000ms;-moz-animation:rippler 1000ms;-o-animation:rippler 1000ms;animation:rippler 1000ms;}@-webkit-keyframes rippler{to{opacity:0;-webkit-transform:scale(2);transform:scale(2);}}@-moz-keyframes rippler{to{opacity:0;-webkit-transform:scale(2);-moz-transform:scale(2);transform:scale(2);}}@-o-keyframes rippler{to{opacity:0;-webkit-transform:scale(2);-o-transform:scale(2);transform:scale(2);}}@keyframes rippler{to{opacity:0;-webkit-transform:scale(2);-moz-transform:scale(2);-o-transform:scale(2);transform:scale(2);}}';
         document.head.appendChild(styleEl);
 })();
